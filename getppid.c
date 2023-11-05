f1b523c57d99f1ceb600dc5cf0dd98bc74e8c56f
lawsonredeye <omoregbeeolawson@gmail.com>
2023-11-05 19:18:04 +0100

    added coding style guidline

--- getppid.c
+++ getppid.c
@@ -2,6 +2,10 @@
 #include <unistd.h>
 #include <sys/types.h>
 
+/**
+ * main - function that prints the parent PID
+ * Return: nothing cos the code was successful
+ */
 int main(void)
 {
 	pid_t myppid = getppid();
