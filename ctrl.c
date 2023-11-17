dacfc45fc49edac9d3917b47d162e30b8a692645
lawsonredeye <omoregbeeolawson@gmail.com>
2023-11-17 23:57:23 +0100

    not fully operational for the shell

--- ctrl.c
+++ ctrl.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
-
+#include "main.h"
+/**
+ * CtrlCHandler - handles Ctrl c
+ * @x: void parameter
+ * Return: nothing
+ */
 void CtrlCHandler(int x)
 {
 	(void)x;
-        printf("\n$ ");
+	printf("\n$ ");
 }
