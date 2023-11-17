9ca5b4bacbc9b2b25169803d7870f670d1350044
lawsonredeye <omoregbeeolawson@gmail.com>
2023-11-17 23:18:03 +0100

    updated header

--- main.h
+++ main.h
@@ -3,6 +3,10 @@
 #include <stdio.h>
 #include <stddef.h>
 
+void CtrlCHandler(int x);
+extern char **environ;
+int _strlen(char *s);
+void mprint(char *c);
 ssize_t _getline(char **lineptr, size_t *n,FILE *stream);
 char *_strtok(char *str, const char *delim);
 #endif
